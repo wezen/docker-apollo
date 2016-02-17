9c8ced7cdb49d1360b202391c6a23ec67ae04659
Eric Rasche <rasche.eric@gmail.com>
2016-02-16 19:45:19 -0600

    Patch in cpt's changes

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
+cp /usr/lib/jvm/java-7-openjdk-amd64/lib/tools.jar /usr/lib/jvm/java-7-openjdk-amd64/jre/lib/ext/tools.jar && \
 . /root/.sdkman/bin/sdkman-init.sh && \
 cd /apollo/ && ./apollo deploy
