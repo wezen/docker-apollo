4047e9b8c2a89989330aa3f957d44ec7017f6d58
Eric Rasche <rasche.eric@gmail.com>
2016-05-27 22:25:14 +0000

    Fix build

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-cp /usr/lib/jvm/java-7-openjdk-amd64/lib/tools.jar /usr/lib/jvm/java-7-openjdk-amd64/jre/lib/ext/tools.jar && \
-cd /apollo/ && ./apollo clean-all && ./apollo deploy
-#cd /apollo/ && ./apollo release
+cd /apollo/ && \
+    cp sample-docker-apollo-config.groovy apollo-config.groovy && \
+    ./apollo clean-all && ./apollo deploy
