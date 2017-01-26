54fcf32a94fd1f8b167de84911aaf67120339abb
Eric Rasche <rasche.eric@gmail.com>
2017-01-26 00:54:57 +0000

    fancier launch.sh

--- launch.sh
+++ launch.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-cp /apollo.war ${CATALINA_HOME}/webapps/${CONTEXT_PATH}.war
+WAR_FILE=${CATALINA_HOME}/webapps/${CONTEXT_PATH}.war
+mkdir -p $(dirname ${WAR_FILE})
+
+cp /apollo.war ${WAR_FILE}
 catalina.sh run
