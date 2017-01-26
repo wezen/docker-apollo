7bd119b7443f8a8522e06f40671608ee8975cc5f
Eric Rasche <rasche.eric@gmail.com>
2017-01-26 02:15:06 +0000

    Do context path right

--- launch.sh
+++ launch.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-WAR_FILE=${CATALINA_HOME}/webapps/${CONTEXT_PATH}.war
-mkdir -p $(dirname ${WAR_FILE})
+# https://tomcat.apache.org/tomcat-8.0-doc/config/context.html#Naming
+FIXED_CTX=$(echo "${CONTEXT_PATH}" | sed 's|/|#|g')
+WAR_FILE=${CATALINA_HOME}/webapps/${FIXED_CTX}.war
 
 cp /apollo.war ${WAR_FILE}
 catalina.sh run
