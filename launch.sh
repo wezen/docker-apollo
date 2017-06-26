25595967db77736c1c4d6f656bd58db858f08d75
Nathan Dunn <ndunn@me.com>
2017-06-26 18:39:12 +0200

    added back user

--- launch.sh
+++ launch.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
+service postgresql start
+service tomcat8 start
+
+#!/bin/bash
+until pg_isready; do
+	echo -n "."
+	sleep 1;
+done
+
+echo "Postgres is up, loading chado"
+su postgres -c 'createdb apollo'
+su postgres -c 'createdb chado'
+su postgres -c 'psql -f /apollo/user.sql'
+
+su postgres -c 'PGPASSWORD=apollo psql -U apollo -h 127.0.0.1 chado -f /chado.sql'
 
 catalina.sh run
