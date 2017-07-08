3d5c901ac0b3abd4320a9ef4930c738b9f668cd4
E Rasche <hxr@hx42.org>
2017-07-08 02:07:13 +0000

    unused

--- launch.sh
+++ launch.sh
@@ -6,23 +6,20 @@ WAR_FILE=${CATALINA_HOME}/webapps/${FIXED_CTX}.war
 rm -rf ${CATALINA_HOME}/webapps/*
 
 cp /apollo/apollo.war ${WAR_FILE}
-if [ ! -z "$WEBAPOLLO_DB_HOST" ]; then
-	export PGUSER=$WEBAPOLLO_DB_USERNAME
-	export PGPASSWORD=$WEBAPOLLO_DB_PASSWORD
-	export DB_CONNECT=$(echo $WEBAPOLLO_DB_URI | sed 's/jdbc://g')
-	while ! psql $DB_CONNECT -l; do
-		echo "Sleeping on DB"
-		sleep 1;
-	done;
+export PGUSER=$WEBAPOLLO_DB_USERNAME
+export PGPASSWORD=$WEBAPOLLO_DB_PASSWORD
+export DB_CONNECT=$(echo $WEBAPOLLO_DB_URI | sed 's/jdbc://g')
+while ! psql $DB_CONNECT -l; do
+	echo "Sleeping on Apollo database"
+	sleep 1;
+done;
 
-	export PGUSER=$WEBAPOLLO_CHADO_DB_USERNAME
-	export PGPASSWORD=$WEBAPOLLO_CHADO_DB_PASSWORD
-	export DB_CONNECT=$(echo $WEBAPOLLO_CHADO_DB_URI | sed 's/jdbc://g')
-	while ! psql $DB_CONNECT -l; do
-		echo "Sleeping on Chado DB"
-		sleep 1;
-	done;
-fi
-
+export PGUSER=$WEBAPOLLO_CHADO_DB_USERNAME
+export PGPASSWORD=$WEBAPOLLO_CHADO_DB_PASSWORD
+export DB_CONNECT=$(echo $WEBAPOLLO_CHADO_DB_URI | sed 's/jdbc://g')
+while ! psql $DB_CONNECT -l; do
+	echo "Sleeping on Chado database"
+	sleep 1;
+done;
 
 catalina.sh run
